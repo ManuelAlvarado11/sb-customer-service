e28104f33f3434fe1a5626899725e25e46d0118a
Manuel Alvarado <manuel.alvarado@bcr.gob.sv>
2024-04-29 11:21:37 -0600

    Add getById and create endpoints

--- src/main/java/com/store/online/customerservice/controller/CustomerController.java
+++ src/main/java/com/store/online/customerservice/controller/CustomerController.java
@@ -1,18 +1,24 @@
 package com.store.online.customerservice.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.store.online.customerservice.entity.Customer;
 import com.store.online.customerservice.entity.Region;
 import com.store.online.customerservice.service.CustomerService;
 
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/customers")
@@ -21,22 +27,69 @@ public class CustomerController {
     CustomerService customerService;
 
     @GetMapping
-    public ResponseEntity<List<Customer>> listAllCustomers(@RequestParam(name = "regionId" , required = false) Long regionId ) {
-        List<Customer> customers =  new ArrayList<>();
-        if (null ==  regionId) {
+    public ResponseEntity<List<Customer>> listAllCustomers(@RequestParam(name = "regionId", required = false) Long regionId) {
+        List<Customer> customers = new ArrayList<>();
+        if (null == regionId) {
             customers = customerService.findCustomerAll();
             if (customers.isEmpty()) {
                 return ResponseEntity.noContent().build();
             }
-        }else{
-            Region Region= new Region();
+        } else {
+            Region Region = new Region();
             Region.setId(regionId);
             customers = customerService.findCustomerByRegion(Region);
-            if ( null == customers ) {
-                return  ResponseEntity.notFound().build();
+            if (null == customers) {
+                return ResponseEntity.notFound().build();
             }
         }
 
-        return  ResponseEntity.ok(customers);
+        return ResponseEntity.ok(customers);
     }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Customer> getCustomer(@PathVariable("id") Long id) {
+        Customer customer = customerService.getCustomer(id);
+        if (customer == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(customer);
+    }
+
+    @PostMapping
+    public ResponseEntity<Customer> createCustomer(@Valid @RequestBody Customer customer, BindingResult result) {
+        if (result.hasErrors()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, this.formatMessage(result) );
+        }
+        Customer customerCreate = customerService.createCustomer(customer);
+        return  ResponseEntity.status(HttpStatus.CREATED).body(customerCreate);
+    }
+
+    private String formatMessage(BindingResult result) {
+        //Map errors result
+        List<Map<String, String>> errors = result.getFieldErrors().stream().map(err -> {
+            Map<String, String> error = new HashMap<>();
+            error.put(err.getField(), err.getDefaultMessage());
+            return error;
+        }).collect(Collectors.toList());
+
+        // Instance Error Message
+        ErrorMessage errorMessage = ErrorMessage.builder()
+                .code("01")
+                .messages(errors)
+                .build();
+
+        // Convert ErrorMessage to String
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonString = "";
+
+        try {
+            jsonString = mapper.writeValueAsString(errorMessage);
+        } catch (JsonProcessingException e){
+            e.printStackTrace();
+        }
+
+        return jsonString;
+    }
+
 }
